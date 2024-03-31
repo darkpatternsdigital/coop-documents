7dd7c0a979df5ae5255eab02839f43b50e444d43
Matt DeKrey <mattdekrey@gmail.com>
2024-03-31 15:32:13 -0500

    Simplify passing headers

--- astro/src/components/sets/documents/article-title.tsx
+++ astro/src/components/sets/documents/article-title.tsx
@@ -1,4 +1,3 @@
-import type { MarkdownHeading } from "astro";
 import type React from "react";
 import { Heading } from "./standard-styles";
 import { useAtomValue } from "jotai";
@@ -6,16 +5,13 @@ import { currentHeadings } from "./current-headings";
 import { getArticleNumber } from "./getArticleNumber";
 import { romanize } from "./romanize";
 
-export function articleTitle(headings: MarkdownHeading[]) {
-    return function ArticleTitle({ id, children, className, style, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
-        const articleNumber = getArticleNumber(useAtomValue(currentHeadings), id ?? '');
-        return (
-            <Heading.h2 id={id} {...props} style={{ ...style, counterSet: `section ${articleNumber}` }}>
-                Article {romanize(articleNumber)}
-                <br/>
-                {children}
-            </Heading.h2>
-        );
-    }
+export function ArticleTitle({ id, children, className, style, ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
+    const articleNumber = getArticleNumber(useAtomValue(currentHeadings), id ?? '');
+    return (
+        <Heading.h2 id={id} {...props} style={{ ...style, counterSet: `section ${articleNumber}` }}>
+            Article {romanize(articleNumber)}
+            <br/>
+            {children}
+        </Heading.h2>
+    );
 }
-
--- astro/src/components/sets/documents/index.tsx
+++ astro/src/components/sets/documents/index.tsx
@@ -1,19 +1,21 @@
 import type { MarkdownHeading } from "astro";
-import { articleTitle } from "./article-title";
+import { ArticleTitle } from "./article-title";
 import { Heading } from "./standard-styles";
 import { elementTemplate } from "../../element-template";
 import { getDefaultStore } from "jotai";
 import { currentHeadings } from "./current-headings";
 import styles from './document-styles.module.css';
 
-export function documentComponents(headings: MarkdownHeading[]) {
+const documentComponents = {
+    h1: Heading.h1,
+    h2: ArticleTitle,
+    p: elementTemplate('Paragraph', 'p', (T) => <T className="my-4" />),
+    ol: elementTemplate('OrderedList', 'ol', (T) => <T className={`my-4 ${styles.subsectionNumbering}`} />),
+    li: elementTemplate('ListItem', 'li', (T) => <T className="my-4" />),
+};
+
+export function getDocumentComponents(headings: MarkdownHeading[]) {
     const store = getDefaultStore();
     store.set(currentHeadings, headings);
-    return {
-        h1: Heading.h1,
-        h2: articleTitle(headings),
-        p: elementTemplate('Paragraph', 'p', (T) => <T className="my-4" />),
-        ol: elementTemplate('OrderedList', 'ol', (T) => <T className={`my-4 ${styles.subsectionNumbering}`} />),
-        li: elementTemplate('ListItem', 'li', (T) => <T className="my-4" />),
-    };
+    return documentComponents;
 }
