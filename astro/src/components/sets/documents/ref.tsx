a62d67a52b6bd28607208cf5e510f41be8ca7cb8
Matt DeKrey <mattdekrey@gmail.com>
2024-04-04 22:22:41 -0500

    Initial tweaks based on real Colorado examples

--- astro/src/components/sets/documents/ref.tsx
+++ astro/src/components/sets/documents/ref.tsx
@@ -4,6 +4,7 @@ import { romanize } from './romanize';
 
 export function Ref({ to, number, section }: { to: string, number?: boolean, section?: string }) {
     const articleNumber = useArticleNumber(createSlug(to));
+    if (isNaN(articleNumber)) return `ERR: Unknown reference (${JSON.stringify({to, number, section})})`;
     if (number) return articleNumber;
     if (section) return <>Section {articleNumber}.{section}</>;
     return <>ARTICLE {romanize(articleNumber)}</>;
