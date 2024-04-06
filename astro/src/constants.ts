6daa87a8fea8caf62ef65272272b0ec710dfade6
Matt DeKrey <mattdekrey@gmail.com>
2024-04-05 22:51:23 -0500

    Update to tentative name

--- astro/src/constants.ts
+++ astro/src/constants.ts
@@ -1,4 +1,4 @@
 /** Per Colorado Statute, the name must contain 'LCA' */
-export const organizationName = '____ LCA';
-export const orgAbbrev = '____';
+export const organizationName = 'Dark Patterns Digital LCA';
+export const orgAbbrev = 'Dark Patterns Digital';
 export const fiscalYearMonth = 'March';
