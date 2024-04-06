24ca69cd330acbd9b3eecae60965aa3a9af20c82
Matt DeKrey <mattdekrey@gmail.com>
2024-04-05 22:53:30 -0500

    Use standard year for fiscal year

--- astro/src/constants.ts
+++ astro/src/constants.ts
@@ -1,4 +1,4 @@
 /** Per Colorado Statute, the name must contain 'LCA' */
 export const organizationName = 'Dark Patterns Digital LCA';
 export const orgAbbrev = 'Dark Patterns Digital';
-export const fiscalYearMonth = 'March';
+export const fiscalYearMonth = 'December';
