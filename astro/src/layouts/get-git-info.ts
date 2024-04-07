10e250edf8b7816724c25834166ea0beb89016e7
Matt DeKrey <mattdekrey@gmail.com>
2024-04-07 07:40:22 -0500

    Date of commit is not helpful

--- astro/src/layouts/get-git-info.ts
+++ astro/src/layouts/get-git-info.ts
@@ -4,12 +4,7 @@ export function getGitInfo() {
     try {
         execSync('git update-index --refresh');
     } catch (error) { }
-    try {
-        const tag = execSync('git describe --tags --exact --dirty --broken').toString().trim();
-        return Promise.resolve(tag);
-    } catch (error) {
-        const result = execSync('git describe --tags --dirty --broken').toString().trim();
-        const date = execSync('git show --no-patch --format=%ci').toString().trim()
-        return Promise.resolve(`${result} @ ${date}`);
-    }
+
+    const result = execSync('git describe --tags --dirty --broken').toString().trim();
+    return Promise.resolve(`${result}`);
 }
