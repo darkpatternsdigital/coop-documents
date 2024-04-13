a98d24015c7accfbfc8e831ac70ddf402ab0a47c
Matt DeKrey <mattdekrey@gmail.com>
2024-04-13 08:29:42 -0500

    Add timestamp to "git info"

--- astro/src/layouts/get-git-info.ts
+++ astro/src/layouts/get-git-info.ts
@@ -1,10 +1,13 @@
 import {execSync} from 'node:child_process';
+import { format } from 'date-fns';
 
 export function getGitInfo() {
     try {
         execSync('git update-index --refresh');
     } catch (error) { }
 
-    const result = execSync('git describe --always --tags --dirty --broken').toString().trim();
-    return Promise.resolve(`${result}`);
+    const gitDescription = execSync('git describe --always --tags --dirty --broken').toString().trim();
+    const unixTimestamp = Number(execSync('git show --no-patch --format=%ct HEAD').toString().trim());
+    const date = new Date(unixTimestamp * 1000);
+    return Promise.resolve(`Last update: ${format(date, "EEEE, do 'of' MMMM, yyyy 'at' h:mmaaa O")} (${gitDescription})`);
 }
