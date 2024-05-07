6aef766da61f24de3436d59d3c1f10cbf1ee74ce
Matt DeKrey <mattdekrey@gmail.com>
2024-05-06 22:27:10 -0500

    Add stories collection

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -1,5 +1,6 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
+
 // Define a `type` and `schema` for each collection
 const documentsCollection = defineCollection({
 	type: 'content',
@@ -8,7 +9,17 @@ const documentsCollection = defineCollection({
 		description: z.string(),
 	})
 });
+
+const storiesCollection = defineCollection({
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+	})
+});
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
 	documents: documentsCollection,
+	stories: storiesCollection,
 };
